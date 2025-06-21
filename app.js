0585a74875e963bba7520e6135143f9002852a56
bhuvan <bhuvanannappa@gmail.com>
2025-06-21 15:30:05 +0530

    DONE SIGNUP PAGE

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.cookie("name", "bhuvan");
-  res.send("done");
+  res.render("index");
 });
 
 app.listen(3000);

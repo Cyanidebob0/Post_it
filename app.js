e4344a1b735d29f32f9619cd6b8bab525ac8588d
bhuvan <bhuvanannappa@gmail.com>
2025-06-21 16:13:22 +0530

    dno ewith signup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const umodel = require("./models/user");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,32 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", (req, res) => {
+  const { username, pass_1, pass_2 } = req.body;
+
+  if (pass_1 !== pass_2) {
+    return res.render("message", {
+      title: "Error",
+      alert: "Passwords do not match!",
+      redirectTo: "/signup",
+      delay: 5000,
+    });
+  }
+
+  // Save user logic here...
+
+  res.render("message", {
+    title: "Success",
+    alert: "User registered successfully!",
+    redirectTo: "/",
+    delay: 5000,
+  });
 });
 
 app.listen(3000);
